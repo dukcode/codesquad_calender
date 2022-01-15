420c45d11fdaf4bd42d86e46e8dcfd46a387da4f
dukcode <duk9741@gmail.com>
2022-01-15 15:35:05 +0900

    Add repeat function

--- source/dukcode/calender/Calender.java
+++ source/dukcode/calender/Calender.java
@@ -11,6 +11,7 @@ class Calender {
 
     public void printSampleCalender() {
         System.out.println("일  월  화  수  목  금  토");
+        System.out.println("--------------------------");
         System.out.println(" 1   2   3   4   5   6   7");
         System.out.println(" 8  10  11  12  13  14  15");
         System.out.println("16  17  18  19  20  21  22");
@@ -19,14 +20,19 @@ class Calender {
 
     public static void main(String[] args)
     {
+        Scanner scanner = new Scanner(System.in);
         Calender cal = new Calender();
 
         cal.printSampleCalender();
+        System.out.println("반복 횟수를 입력하세요.");
+        int repeat = scanner.nextInt();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("달을 입력하세요");
-        int month = scanner.nextInt();
-        System.out.printf("%d월은 %d일까지 있습니다.%n", month, cal.getMaxDaysOfMonth(month));
+        for (int i = 0; i < repeat; ++i) {
+            System.out.println("달을 입력하세요");
+            int month = scanner.nextInt();
+            System.out.printf("%d월은 %d일까지 있습니다.%n", month, cal.getMaxDaysOfMonth(month));
+        }
+
         scanner.close();
     }
 }
