fce52f70606c58f57caa77dd8d199f6d86b74b3f
dukcode <duk9741@gmail.com>
2022-01-17 01:09:38 +0900

    add getWeekDay method

--- source/dukcode/calender/Calender.java
+++ source/dukcode/calender/Calender.java
@@ -1,22 +1,44 @@
 package dukcode.calender;
 
 class Calender {
-    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
-    private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
+    private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
+    private static final int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
 
     public boolean isLeapYear(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 != 0) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             return true;
         }
 
-        return true;
+        return false;
     }
     public int getMaxDaysOfMonth(int year, int month) {
         if (isLeapYear(year)) {
-            return LEAP_MAX_DAYS[month - 1];
+            return LEAP_MAX_DAYS[month];
         } else {
-        return MAX_DAYS[month - 1];
+        return MAX_DAYS[month];
         }
+    }
+
+    private int getWeekDay(int year, int month, int day) {
+        final int STANDARD_YEAR = 1970;
+        final int STANDARD_WEEKDAY = 4; // weekDay of 1970/1/1 is TH
+
+        int count = 0;
+        int delta = 0;
+
+        for (int y = STANDARD_YEAR; y < year; ++y) {
+            delta = isLeapYear(y) ? 366 : 365;
+            count += delta;
+        }
+
+        for (int m = 1; m < month; ++m) {
+            delta = getMaxDaysOfMonth(year, m);
+            count += delta;
+        }
+
+        count += day - 1;
+
+        return (count + STANDARD_WEEKDAY) % 7;
     }
 
     public void printCalender(int year, int month) {
@@ -24,15 +46,32 @@ class Calender {
         System.out.println("일  월  화  수  목  금  토");
         System.out.println("--------------------------");
 
+        // get weekDay automatically
+        int weekDay = getWeekDay(year, month, 1);
         int maxDay = getMaxDaysOfMonth(year, month);
 
+        // print blank space
+        for (int i = 0; i < weekDay; ++i) {
+            System.out.print("    ");
+        }
         for (int day = 1; day <= maxDay; ++day) {
             System.out.printf("%2d  ", day);
-            if (day % 7 == 0) {
+            if ((day + weekDay) % 7 == 0) {
                 System.out.println();
             }
         }
         System.out.println();
+        System.out.println();
+    }
+
+    // Simple test code here
+    public static void main(String[] args) {
+        Calender cal = new Calender();
+        System.out.println(cal.getWeekDay(1970, 1, 1) == 3);
+        System.out.println(cal.getWeekDay(1971, 1, 1) == 4);
+        System.out.println(cal.getWeekDay(1972, 1, 1) == 5);
+        System.out.println(cal.getWeekDay(1973, 1, 1) == 0);
+        System.out.println(cal.getWeekDay(1974, 1, 1) == 1);
     }
 
 }
--- source/dukcode/calender/Prompt.java
+++ source/dukcode/calender/Prompt.java
@@ -4,30 +4,46 @@ import java.util.Scanner;
 import dukcode.calender.Calender;
 
 public class Prompt {
-    private final static String PROMPT = "cal > ";
+    final static String PROMPT = new String("CAL > ");
+
+    /**
+     * @param week 요일명
+     * @return 0~ 6 (0 = Sunday, 6 = Saturday)
+     */
+    public int parsDay(String week) {
+        if (week.equals("SU")) return 0;
+        else if (week.equals("MO")) return 1;
+        else if (week.equals("TU")) return 2;
+        else if (week.equals("WE")) return 3;
+        else if (week.equals("TH")) return 4;
+        else if (week.equals("FR")) return 5;
+        else if (week.equals("SA")) return 6;
+        else return 0;
+    }
 
     public void runShell() {
-        String PROMPT = new String("cal> ");
         Scanner scanner = new Scanner(System.in);
         Calender cal = new Calender();
 
-        int month = -1;
-        int year = -1;
+        int month = 1;
+        int year = 2017;
         while (true) {
-            System.out.println("년도를 입력하세요");
-            System.out.print("YEAR > ");
+            System.out.println("년도를 입력하세요 (exit : -1)");
+            System.out.print(PROMPT);
             year = scanner.nextInt();
-            System.out.println("달을 입력하세요");
-            System.out.print("MONTH > ");
-            month = scanner.nextInt();
-
-            if (month == -1) {
+            if (year == -1) {
                 break;
             }
 
-            if (month > 12) {
-                continue;
+            System.out.println("달을 입력하세요");
+            System.out.print(PROMPT);
+            month = scanner.nextInt();
+
+            if (month < 1 || 12 < month) {
+                System.out.println("잘못된 입력입니다.");
+                break;
             }
+
             cal.printCalender(year, month);
         }
 
